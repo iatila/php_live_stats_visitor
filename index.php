9e3a875fb3efa642c30eef1117fc2b3ef2677a02
İsmail Atila <wildwamqire@gmail.com>
2024-08-18 18:12:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,14 +3,15 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 $page = isset($_GET['page']) ? strip_tags(addslashes($_GET['page'])): '';
 $action = isset($_GET['action']) ? strip_tags(addslashes($_GET['action'])): '';
-if (($_SERVER['PHP_AUTH_USER'] != '001') || ($_SERVER['PHP_AUTH_PW'] != '007')){
+require __DIR__.'/src/config.php';
+if (($_SERVER['PHP_AUTH_USER'] != $cnf['user']) || ($_SERVER['PHP_AUTH_PW'] != $cnf['pass'])){
     header('www-Authenticate: Basic realm="Yetkisiz erisim');
     header('HTTP/1.0 401 Unauthorized');
     echo "Yetkisiz erisim..";
     exit;
 }
 
-require __DIR__.'/src/config.php';
+
 
 if ($page == 'ajax'){
     require __DIR__.'/inc/ajax.php';
